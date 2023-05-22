e876045ed9ed3291c90da1507b6339dbd7177176
bilskyi <bilskyioleksandr@outlook.com>
2023-05-22 17:02:32 +0300

    register & login / #13

--- poet_flow/home/urls.py
+++ poet_flow/home/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('', welcome, name='home'),
     path('home/', home, name='home'),
+    path('register/', register_user, name='register'),
+    path('login/', login_user, name='login'),
 ]
--- poet_flow/home/views.py
+++ poet_flow/home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegister
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def welcome(request):
@@ -7,3 +10,27 @@ def welcome(request):
 
 def home(request):
     return render(request, 'home/home.html')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    form = UserRegister()
+    return render(request, 'home/register.html', {'form': form})
+
+
+def login_user(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data.get('username'),
+                            password=form.cleaned_data.get('password'))
+            login(request, user)
+            return redirect('home')
+    form = AuthenticationForm()
+    return render(request, 'home/login.html', {'form': form})
