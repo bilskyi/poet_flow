4d5242ecd962e3ebd41282a3ea6d6ddd588dae63
bilskyi <bilskyioleksandr@outlook.com>
2023-05-22 17:58:48 +0300

    header / #14

--- poet_flow/home/urls.py
+++ poet_flow/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', welcome, name='home'),
+    path('', welcome, name='welcome'),
     path('home/', home, name='home'),
     path('register/', register_user, name='register'),
     path('login/', login_user, name='login'),
