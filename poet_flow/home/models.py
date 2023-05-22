f86cd5afc98260f64e7117e206ff66e5197ade36
bilskyi <bilskyioleksandr@outlook.com>
2023-05-22 10:57:40 +0300

    connect to postgresql / #12

--- poet_flow/home/models.py
+++ poet_flow/home/models.py
@@ -1,7 +1,2 @@
 from django.db import models
 
-class Poem(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    #author = models.ForeignKey()
-    #tags
