b8e296d920c81aaf12072a0e1c6de99d7a2f388a
bilskyi <bilskyioleksandr@outlook.com>
2023-05-23 13:31:30 +0300

    register form / #17

--- poet_flow/home/forms.py
+++ poet_flow/home/forms.py
@@ -4,7 +4,10 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class UserRegister(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Your Email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repeat your password'}))
 
     class Meta:
         model = User
