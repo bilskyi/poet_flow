2b95ec3a49b0b3e2f8c55a1e65e583cdb44c80d1
bilskyi <bilskyioleksandr@outlook.com>
2023-05-26 13:00:53 +0300

    adding form / #23

--- poet_flow/home/forms.py
+++ poet_flow/home/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm  
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from .models import Poem  
 
 
 class UserRegister(UserCreationForm):
@@ -12,3 +14,9 @@ class UserRegister(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class AddPost(forms.ModelForm):
+    class Meta:
+        model = Poem
+        fields = [ 'title', 'content', 'tags']
