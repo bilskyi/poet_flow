e11bb397cecdef369d813b8d6311b72f1a2277cb
bilskyi <bilskyioleksandr@outlook.com>
2023-05-29 15:40:01 +0300

    fixing models

--- poet_flow/home/models.py
+++ poet_flow/home/models.py
@@ -1,23 +1,24 @@
+from typing import Iterable, Optional
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     description = models.TextField()
-    
-    
-    
-#     def __str__(self) -> str:
-#         return self.user.username
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.user.username
+
 
 class Poet(models.Model):
     name = models.CharField(max_length=255)
     biography = models.TextField()
     birth_date = models.DateField()
     death_date = models.DateField(null=True, blank=True)
-    poems = models.ManyToManyField('Poem')
+    poems = models.ManyToManyField('Poem', blank=True)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
 
     def __str__(self) -> str:
@@ -25,18 +26,21 @@ class Poet(models.Model):
 
 
 class Poem(models.Model):
-    user_author = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user_author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     poet_author = models.ForeignKey(Poet, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=50)
     content = models.TextField()
     date = models.DateField(auto_now_add=True)
-    tags = models.ManyToManyField('Tags')
+    tags = models.ManyToManyField('Tags', blank=True)
 
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
     def __str__(self) -> str:
         return self.title
-    
 
 
 class Tags(models.Model):
