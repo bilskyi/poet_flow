56cd951f1b6ba826ac5434a1e79ffb11350cca54
bilskyi <bilskyioleksandr@outlook.com>
2023-06-05 19:39:37 +0300

    fixing poet_detail template / #48

--- poet_flow/explore/urls.py
+++ poet_flow/explore/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', explore, name='explore'),
+    path('<slug:slug>/', poet_detail, name='poet-detail'),
 ]
--- poet_flow/explore/views.py
+++ poet_flow/explore/views.py
@@ -5,3 +5,8 @@ from home.models import Poet
 def explore(request):
     poets = Poet.objects.all()
     return render(request, 'explore/explore.html', {'poets': poets})
+
+
+def poet_detail(request, slug):
+    poet = Poet.objects.get(slug=slug)
+    return render(request, 'explore/poet_detail.html', {'poet': poet})
