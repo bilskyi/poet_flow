781669671ee89d502597ab39c122c9d1f0c330ed
bilskyi <bilskyioleksandr@outlook.com>
2023-06-08 22:37:12 +0300

    adding backends (abiity to log in) / #55

---
+++ poet_flow/users/backends.py
@@ -0,0 +1,24 @@
+from users.models import User
+from django.db.models import Q
+
+
+class AuthBackend(object):
+    supports_object_permissions = True
+    supports_anonymous_user = False
+    supports_inactive_user = False
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+        
+    def authenticate(self, request, username, password):
+        try:
+            user = User.objects.get(
+                Q(username=username) | Q(email=username) | Q(phone=username)
+            )
+        except User.DoesNotExist:
+            return None
+        
+        return user if user.check_password(password) else None
