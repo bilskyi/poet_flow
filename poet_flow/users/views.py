e5e05fd17ee17c54c94f8ca65a2591ebf54c07f1
bilskyi <bilskyioleksandr@outlook.com>
2023-06-12 22:49:34 +0300

    fixing permissions / #65

--- poet_flow/users/views.py
+++ poet_flow/users/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from .models import User
 from .forms import UpdateUserForm
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+
 
 
 def profile(request, slug):
     user = User.objects.get(slug=slug)
     return render(request, 'users/profile.html', {'user': user})
 
-
+@login_required
 def settings(request):
-    form = UpdateUserForm()
+    if request.user.id != request.GET.get('user_id'):
+        raise PermissionDenied
+    
     if request.method == 'POST':
         form = UpdateUserForm(request.POST, instance=request.user)
         if form.is_valid():
