9e0ab016a4072645e471bd10e96b18d96beba4c3
bilskyi <bilskyioleksandr@outlook.com>
2023-06-21 15:59:23 +0300

    refactoring explore / #78

--- poet_flow/explore/urls.py
+++ poet_flow/explore/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', explore, name='explore'),
+    path('poets/', classic_poets, name='explore'),
+    path('users/', user_poets, name='user_poets'),
     path('<slug:slug>/', poet_detail, name='poet-detail'),
 ]
--- poet_flow/explore/views.py
+++ poet_flow/explore/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
 from home.models import Poet
+from users.models import User
 
 
-def explore(request):
+def classic_poets(request):
     poets = Poet.objects.all()
-    return render(request, 'explore/explore.html', {'poets': poets})
+    selected_poets = 'classic'
+
+    context = {
+        'poets': poets,
+        'selected_poets': selected_poets,
+    }
+
+    return render(request, 'explore/classic_poets.html', context=context)
+
+
+def user_poets(request):
+    poets = User.objects.all()
+    selected_poets = 'user'
+
+    context = {
+        'poets': poets,
+        'selected_poets': selected_poets,
+    }
+
+    return render(request, 'explore/user_poets.html', context=context)
 
 
 def poet_detail(request, slug):
-    poet = Poet.objects.get(slug=slug)
+    
+    try:
+        poet = User.objects.get(slug=slug)
+        
+    except User.DoesNotExist:
+        poet = Poet.objects.get(slug=slug)
+
     return render(request, 'explore/poet_detail.html', {'poet': poet})
