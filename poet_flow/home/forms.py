2c5b0554a165b0cb41f2b3cbb8f52afaf0bb0515
bilskyi <bilskyioleksandr@outlook.com>
2023-08-02 23:54:34 +0300

    beginnig the search bar

--- poet_flow/home/forms.py
+++ poet_flow/home/forms.py
@@ -1,7 +1,5 @@
-from typing import Any
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
-from .models import Poem, Tags
-from users.models import User
-from django.contrib.auth import authenticate
+
+
+class SearchForm(forms.Form):
+    query = forms.TextInput()
--- poet_flow/home/views.py
+++ poet_flow/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from .forms import *
 
 from django.contrib.auth.forms import AuthenticationForm
 from users.models import User
@@ -8,7 +9,8 @@ from users.models import User
 from .models import ClassicPoem, UserPoem
 
 def welcome(request):
-    return render(request, 'home/welcome.html')
+    bar = SearchForm()
+    return render(request, 'home/welcome.html', {'bar': bar})
 
 
 def home(request):
@@ -22,3 +24,8 @@ def view_detail(request, poet_slug, poem_slug):
     except:
         poem = ClassicPoem.objects.get(slug=poem_slug)
     return render(request, 'home/view_detail.html', {'poem': poem})
+
+
+def search(request):
+    form = SearchForm()
+    return form
