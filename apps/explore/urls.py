ca2942972f3a02e8a4b06a31f2e79add2a4e2b25
bilskyi <bilskyioleksandr@outlook.com>
2023-08-09 11:49:14 +0300

    improving UI/UX & fixed url naming

--- apps/explore/urls.py
+++ apps/explore/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('poets/', classic_poets, name='explore'),
     path('users/', user_poets, name='user_poets'),
-    path('<slug:slug>/', poet_detail, name='poet-detail'),
+    path('<slug:slug>/', poet_detail, name='poet_detail'),
 ]
